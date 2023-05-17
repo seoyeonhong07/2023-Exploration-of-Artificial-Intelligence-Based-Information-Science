502998b581591ce0fac62d0f8adff5af470e4efa
seoyeon77 <nac5_230105@gclass.ice.go.kr>
2023-05-17 21:07:31 +0900

    week09_class_object04

--- week09_class_object03.py
+++ week09_class_object03.py
@@ -8,13 +8,24 @@ class Pokemon:
     def say(self):
         print(f"I'm a pokemon. My name is {self.name}")
 
+    def attack(self):
+        print(f"{self.name} launches an area attack.")
+
 
 class Pikachu(Pokemon):   # is-a relationship
-    pass
+    def attack(self):
+        print(f"{self.name} launches an area-of-effect electric attack.")
 
 
 class Squirtle(Pokemon):
-    pass
+    def attack(self):
+        print(f"{self.name} casts a wide-area water cannon attack.")
+
+
+class Charizard(Pokemon):
+    def attack(self):   # derived class method, override
+        super().attack()   # base class method
+        print(f"{self.name} casts a wide-area fire attack.")
 
 
 class Digimon:
@@ -22,16 +33,17 @@ class Digimon:
 
 if __name__ == "__main__":
     #pikachu.say()
-
     pikachu = Pikachu("pikachu", 1, 35)
-
     pikachu.say()
-
     squirtle = Squirtle("squirtle", 1, 44)
-    charizard = Pokemon("charizard", 36, 78)
+    charizard = Charizard("charizard", 36, 78)
     charizard.say()
+
+    charizard.attack()
+    pikachu.attack()
+    squirtle.attack()
 
     print(squirtle.name)
     print(pikachu.name)
     print(issubclass(Squirtle, Pokemon))
-    print(issubclass(charizard, Pokemon))
+    print(issubclass(Charizard, Pokemon))
