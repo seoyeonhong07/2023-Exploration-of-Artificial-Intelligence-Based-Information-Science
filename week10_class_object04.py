aacc0f8438fe26516371a76ff5811b8f089e8d00
seoyeon77 <nac5_230105@gclass.ice.go.kr>
2023-05-24 19:31:30 +0900

    week10 class&object 06

--- week10_class_object04.py
+++ week10_class_object04.py
@@ -13,10 +13,19 @@ class Pokemon(PrettyMixin):
         self.hp = hp
         self.level = level
 
+    def info(self):
+        print("=================")
+        print(f'Name : {self.name}')
+        print(f'Hp : {self.hp}')
+        print(f'Level : {self.level}')
+        print("=================")
+
+
 
 if __name__ == "__main__":
     p1 = Pokemon("pikachu", 35, 1)
     p2 = Pokemon("squirtle", 40, 1)
+    p2.info()   #Pokemon.info(p2)
     p1.dump()
     p2.dump()
 
